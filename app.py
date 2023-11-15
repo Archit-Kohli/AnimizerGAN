b9627074107e80c745d447417a9f97c539f036f9
Archit Kohli <ArchitKohli@users.noreply.huggingface.co>
2023-11-15 11:18:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -180,5 +180,5 @@ def animize(img):
 
     return out.numpy()
 
-interface = gr.Interface(fn=animize, inputs="image", outputs="image", examples=['./example1.jpg','./example2.png','./example3.jpg','./example4.jpg'])
+interface = gr.Interface(fn=animize, inputs="image", outputs="image", examples=['./example2.jpg','./example3.png','./example1.jpg','./example4.jpg'])
 interface.launch(share=True)
